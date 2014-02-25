f79692d794b3a5cfd32f4cd47a8aa7790a686173
CleezyITP <ckv214@nyu.edu>
2014-02-25 16:21:07 -0500

    New Unity Project

---
+++ New Unity Project/Assets/NewBehaviourScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class NewBehaviourScript : MonoBehaviour {
+	string[] days = new string[] {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+	//int[] horrible=new int[10];
+
+	// Use this for initialization
+	void Start () {
+//		for(int i = 0; i < 10; i++) {
+//			horrible[i] = i;
+//		}
+		Debug.Log ("weekdays");
+
+		for(int i = 0; i < 7; i++) {
+			Debug.Log (days[i]);
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
